ea17199ac510f6f442bb23353ce1161ef18cbef6
Tobi Samuele <ufomatobi@gmail.com>
2022-04-14 19:32:26 +0100

    Fixed Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+import {
+  FaBars,
+  FaFacebook,
+  FaInstagram,
+  FaTimes,
+  FaYoutube,
+} from "react-icons/fa";
+import Card from "../components/card";
+
+const plans = [{ name: "", price: "", description: "", content: {} }];
 
 export default function Home() {
+  const [show, setShow] = useState(false);
+
+  const toggleNav = () => setShow(!show);
+
   return (
     <div>
       <Head>
@@ -11,97 +26,181 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <nav className="container mx-auto p-6 shadow-md">
+      <nav className="relative mx-auto px-10 shadow-sm md:px-16">
         <div className="flex items-center justify-between">
-          <div>
-            <h1 className="font-bold">RTW</h1>
+          <Image src="/images/logo.png" width={140} height={100} alt="logo" />
+
+          {/* Hamburger Icon*/}
+          <div
+            className="text-xl  text-fuchsia-600 md:hidden"
+            onClick={toggleNav}
+          >
+            {show ? <FaTimes /> : <FaBars />}
           </div>
-          <div className="hidden md:flex space-x-5">
+
+          {/* NavLinks */}
+          <div className="hidden font-bold text-fuchsia-600 space-x-5 md:flex">
             <Link href="/">
               <a>About</a>
             </Link>
             <Link href="/">
-              <a>Plans</a>
+              <a>Programs</a>
             </Link>
             <Link href="/">
               <a>Contact</a>
             </Link>
           </div>
         </div>
+
+        {/* Mobile Menu */}
+        <div className="md:hidden">
+          {show ? (
+            <div className="absolute flex flex-col items-center self-end w-11/12 left-0 right-0 top-20 mx-auto mt-10 py-8 px-6 space-y-6 z-50 bg-white text-fuchsia-600 font-bold drop-shadow-md sm:self-center">
+              <Link href="/">
+                <a>About</a>
+              </Link>
+              <Link href="/">
+                <a>Programs</a>
+              </Link>
+              <Link href="/">
+                <a>Contact</a>
+              </Link>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
       </nav>
 
-      <section>
-        <div className="container flex flex-col md:flex-row items-center mx-auto mt-10 px-6">
-          {/* left side */}
-          <div className="flex-1 flex flex-col space-y-4 py-6">
-            <h1 className="max-w-md text-3xl font-extrabold text-center md:text-4xl md:text-left">
-              A Transformational, Revolutionary and Holistic Fitness and
-              Wellness Program designed for Christian Women
-            </h1>
-            <p className="max-w-md text-center text-amber-500 md:text-left">
-              A program designed for Christian Women who desire fitness and
-              wellness for their Spirit, Soul and Body.
-              <br />1 Thessalonians 5:23a &quot;...and may your spirit and soul
-              be kept complete...&quot;
-            </p>
-            <div className="flex justify-center md:justify-start">
+      <main>
+        {/* Heading */}
+        <section>
+          <div className=" flex flex-col-reverse items-center mx-auto mt-12 px-16 md:flex-row">
+            {/* left side */}
+            <div className="flex-1 flex flex-col space-y-4 py-6">
+              <h1 className="text-2xl uppercase font-extrabold text-fuchsia-600 text-center md:px-0 md:text-3xl md:text-left">
+                A Transformational, Revolutionary and Holistic Fitness and
+                Wellness Program designed for Christian Women
+              </h1>
+              <p className="text-center text-rtw-4 md:text-left">
+                A program designed for Christian Women who desire fitness and
+                wellness of their spirit, soul and body.
+                {/* <br />1 Thessalonians 5:23a &quot;...and may your spirit and soul
+              be kept complete...&quot; */}
+              </p>
+              <div className="flex justify-center md:justify-start">
+                <Link href="/">
+                  <a className="px-5 py-2 rounded-md inline-block text-center bg-fuchsia-600 font-medium text-white hover:bg-fuchsia-700">
+                    Get Started
+                  </a>
+                </Link>
+              </div>
+            </div>
+
+            {/* right side */}
+            <div className="flex-1">
+              <Image
+                src="/images/section.png"
+                width={600}
+                height={400}
+                alt=""
+              />
+            </div>
+          </div>
+        </section>
+
+        {/* About Section */}
+        <section>
+          <div className="flex justify-between flex-col items-center mt-16 mx-auto  px-16 md:flex-row">
+            {/* left side */}
+            <div className="flex-1">
+              <Image src="/images/hero.png" width={600} height={428} alt="" />
+            </div>
+
+            {/* right side */}
+            <div className="flex-1 flex flex-col space-y-4 my-14 text-center md:ml-7 md:text-left">
+              <h3 className="text-fuchsia-500 font-semibold">ABOUT US</h3>
+              <h2 className="text-3xl font-extrabold uppercase text-fuchsia-600 md:text-2xl ">
+                Building habits for sustainable fitness and wellness
+              </h2>
+              <p className="text-rtw-4">
+                At Run2Win, we believe that fitness is much more than the number
+                on the scale; it is not about perfection but consistency; It is
+                not about being skinny but about being fit, healthy, strong and
+                ultimately the best version of yourself.
+              </p>
+              <div className="flex justify-center md:justify-start">
+                <Link href="/">
+                  <a className="px-5 py-2 rounded-md inline-block text-center bg-fuchsia-600 font-medium text-white hover:bg-fuchsia-700">
+                    Learn More
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* Programs Section */}
+        <section>
+          <div className="max-w-4xl mt-12 mx-auto px-10 text-center">
+            <h2 className="text-fuchsia-600 text-2xl text-center font-extrabold">
+              Our Programs
+            </h2>
+            <h4 className="mt-2">
+              Run2Win Fitness provides programs for individuals and groups
+            </h4>
+            {/* Cards Container */}
+
+            <div className="flex flex-col justify-center mt-10 space-y-8 md:flex-row md:space-x-5">
+              <Card />
+            </div>
+          </div>
+        </section>
+
+        {/* CTA Section */}
+        <section>
+          <div className="flex flex-col justify-between items-center bg-fuchsia-600 mx-auto mt-12 px-10 py-24 md:flex-row ">
+            <h3 className="font-bold text-xl text-white text-center md:text-2xl md:text-left">
+              Begin the journey to becoming the best version of you
+            </h3>
+            <div>
               <Link href="/">
-                <a className="px-6 py-3 rounded-md inline-block text-center bg-amber-600 font-medium text-white hover:bg-amber-700">
+                <a className="mt-5 px-5 py-2 rounded-md inline-block text-center bg-white font-medium text-fuchsia-600 md:mt-0">
                   Get Started
                 </a>
               </Link>
             </div>
           </div>
+        </section>
+      </main>
 
-          {/* right side */}
-          <div className="flex-1">
-            <Image src="/images/hero.jpg" width={600} height={400} alt="" />
+      <footer>
+        <div>
+          <div className="flex justify-center text-fuchsia-500 font-bold space-x-5 mt-10">
+            <Link href="/">
+              <a>About</a>
+            </Link>
+            <Link href="/">
+              <a>Programs</a>
+            </Link>
+            <Link href="/">
+              <a>Contact</a>
+            </Link>
+          </div>
+
+          <div className="flex justify-center p-5 text-fuchsia-600 text-2xl space-x-6">
+            <a href="https://facebook.com/run2winfitness">
+              <FaFacebook />
+            </a>
+            <a href="https://www.instagram.com/runtowinfitness/">
+              <FaInstagram />
+            </a>
+            <a href="https://www.youtube.com/channel/UCe-vpacqipk4VR8jlPGCbNA">
+              <FaYoutube />
+            </a>
           </div>
         </div>
-      </section>
-
-      <section>
-        <div className="container mx-auto mt-10 px-6">
-          <div className="grid grid-cols-2 gap-3 sm:grid-cols-3">
-            <div className="h-30 w-30 p-3 bg-amber-300 rounded-md">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-                officia a, expedita, sapiente velit repellendus odio pariatur
-                aut ad excepturi fugit doloremque distinctio totam adipisci
-                aliquam quos maxime perspiciatis repudiandae.
-              </p>
-            </div>
-            <div className="h-30 w-30 p-3 bg-amber-300 rounded-md">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-                officia a, expedita, sapiente velit repellendus odio pariatur
-                aut ad excepturi fugit doloremque distinctio totam adipisci
-                aliquam quos maxime perspiciatis repudiandae.
-              </p>
-            </div>
-            <div className="h-30 w-30 p-3 bg-amber-300 rounded-md">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-                officia a, expedita, sapiente velit repellendus odio pariatur
-                aut ad excepturi fugit doloremque distinctio totam adipisci
-                aliquam quos maxime perspiciatis repudiandae.
-              </p>
-            </div>
-            <div className="h-30 w-30 p-3 bg-amber-300 rounded-md">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-                officia a, expedita, sapiente velit repellendus odio pariatur
-                aut ad excepturi fugit doloremque distinctio totam adipisci
-                aliquam quos maxime perspiciatis repudiandae.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <main></main>
-
-      <footer></footer>
+      </footer>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "rtw-1": "#A904BF",
+        "rtw-2": "#C004D9",
+        "rtw-3": "#ECBBF2",
+        "rtw-4": "#261B14",
+        "rtw-5": "#F2F2F2",
+      },
+    },
   },
   plugins: [],
 };
